2573ddc34553e043e44240bd603a88445107f2e6
sofia-012 <sjalilbestard@udesa.edu.ar>
2026-03-31 21:05:13 -0300

    metricas.py

---
+++ src/metricas.py
@@ -0,0 +1,79 @@
+def calcular_tiempo_total(datos):
+    '''
+    Calcula la suma total del tiempo de uso de todos los registros.
+
+    Parameters
+    ----------
+    datos : list
+        Lista de diccionarios con los registros del archivo CSV
+
+    Returns
+    -------
+    total : float
+        Tiempo total de uso (en minutos)
+
+    '''
+    total= 0
+    for dato in datos:
+        total+= dato['tiempo_uso']
+    return total
+    
+    
+def calcular_promedio_uso(datos):
+    '''
+    Calcula el promedio de tiempo de uso de los registros.
+
+    Parameters
+    ----------
+    datos : list
+        Lista de diccionarios con los registros del archivo CSV
+
+    Returns
+    -------
+    promedio : float
+        Promedio de tiempo de uso por registro
+
+    '''
+    cantidad_registros= 0
+    tiempo_total= 0
+    for dato in datos:
+        tiempo_total+= dato['tiempo_uso']
+        cantidad_registros += 1
+    promedio= tiempo_total / cantidad_registros
+    return promedio
+
+
+def calcular_uso_por_app(datos):
+    '''
+    Calcula el tiempo total de uso agrupado por cada aplicación.
+
+    Parameters
+    ----------
+    datos : list
+        Lista de diccionarios con los registros del archivo CSV
+
+    Returns
+    -------
+    diccionario : dict
+        Diccionario con el formato {"nombre_app": tiempo_total}
+
+    '''
+    diccionario= {}
+    for dato in datos:
+        app = dato['app']
+        if app not in diccionario:
+            diccionario[app] = 0
+        diccionario[app] += dato['tiempo_uso']
+            
+    return diccionario
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
