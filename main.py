0711271dcac7c8cfd1c9507b405b7620a64d5658
guadalupemerke <gmerke@udesa.edu.ar>
2026-04-05 20:06:31 -0300

    Add data validation before calculations

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import src.carga_datos as c
 import src.metricas as m
+import src.validacion_datos as v
 
 datos = c.cargar_datos("datos/datos.csv")
+datos_validos = []
 
-print("Tiempo total:", m.calcular_tiempo_total(datos))
-print("Promedio:", m.calcular_promedio_uso(datos))
-print("Uso por app:", m.calcular_uso_por_app(datos))
+for d in datos:
+    if v.validar_registro(d):
+        datos_validos.append(d)
 
+tiempo_total = m.calcular_tiempo_total(datos_validos)
+promedio = m.calcular_promedio_uso(datos_validos)
+uso_por_app = m.calcular_uso_por_app(datos_validos)
+
+print("Tiempo total:", tiempo_total)
+print("Promedio de uso:", promedio)
+print("Uso por app:", uso_por_app)
 
