cf7306a5955ebd0afc74ceb9b19c34fcc98646b1
guadalupemerke <gmerke@udesa.edu.ar>
2026-04-05 19:56:50 -0300

    progrma principal

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import src.carga_datos as c
+import src.metricas as m
+
+datos = c.cargar_datos("datos/datos.csv")
+
+print("Tiempo total:", m.calcular_tiempo_total(datos))
+print("Promedio:", m.calcular_promedio_uso(datos))
+print("Uso por app:", m.calcular_uso_por_app(datos))
+
 
