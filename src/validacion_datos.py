79f6e48aeab43b026cda72ab3770035e1915d003
mberazategui <mberazategui@udesa.edu.ar>
2026-04-14 21:16:07 -0300

    Actualizo validacion

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -12,18 +12,31 @@ def validar_registro(dato):
     bool
         True si el registro es válido, False en caso contrario.
     '''
+#Verifica que cada valor lista en lista_diccionarios tenga al menos un elemento
+#si no tiene nada entonces se rompe. 
 
     try:
-        if dato['cantidad_uso'] < 0:
+        if len(dato["fecha"])== 0:
             return False
-        
-        if dato['tiempo_uso'] < 0:
+        if len(dato["app"])== 0:
             return False
-        
-        if len(dato['app']) == 0:
+        if len(dato["cantidad_uso"])== 0:
             return False
-        
-        return True
+        if len(dato["tiempo_uso"])== 0:
+            return False
     
-    except:
-        return False
+        for cantidad_uso in dato["cantidad_uso"]:
+            if cantidad_uso < 0:
+                return False 
+# Si los numeros guardados en tiempo_uso o cantidad_uso es ngativo se rompe.     
+
+        for tiempo_uso in dato["tiempo_uso"]:
+            if tiempo_uso < 0:
+                return False     
+        return True  
+    except KeyError:
+           return False
+    
+    except TypeError:
+           return False
+    
