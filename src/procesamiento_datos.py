d689331b32057adf6a000e0d53aa7c440ff96542
mberazategui <mberazategui@udesa.edu.ar>
2026-04-21 20:53:42 -0300

    Agrego validaciones a la funcion filtrar_datos

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -15,8 +15,30 @@ def  filtrar_por_participante(datos, id_participante):
         Lista con solo los registros del participante solicitado
 
     '''
+    if datos == None:
+        print("Error critico: error- no hay datos, ubicacion:filtrar_por_participante")
+        return []
+    if len(datos)== 0:
+        print("Error la lista esta vacia, ubicacion:filtrar_por_participante")
+        return []
+    if id_participante <=0:
+        print("error critico, el id debe ser positivo, ubicacion:filtrar_por_participante")
+        return[]
     lista= []
+
     for dato in datos:
+        if "id_participante" not in dato:
+            print("error, id_participante esta vacio, ubicacion:filtrar_por_participante")
+            return[]
+        if dato["id_participante"]<=0:
+           print("error critico, error: el id  debe ser positivo, ubicacion: filtrar_por_participante")
+           return[]
+        if dato["id_participante"]== None:
+           print("error critico, id_participante esta vacio, ubicacion:filtrar_por_participante")
+           return[]
+           
         if dato['id_participante']== id_participante:
             lista.append(dato)
+    if len(lista)== 0:
+        print("Error critico, la lista esta vacia el participante no esta en la lista,ubicacion: filtrar_por_participante")
     return lista
